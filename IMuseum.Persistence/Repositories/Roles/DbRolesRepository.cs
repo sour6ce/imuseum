4123e6e0d65db6ad205bb7111e25d818b78a1821
Sour_ce <gabrielhdezrodriguez@gmail.com>
2022-06-23 16:33:27 -0400

    fix: Rollback Context rename and injection
    (cherry picked from commit f0836faa0d12e1fb83783b6dbe9526879d689f29)

--- IMuseum.Persistence/Repositories/Roles/DbRolesRepository.cs
+++ IMuseum.Persistence/Repositories/Roles/DbRolesRepository.cs
@@ -13,7 +13,7 @@ public class DbRolesRepository : DbRepository<Role>, IRolesRepository
 #pragma warning disable 8603
         using (var scope = this.serviceProvider.CreateScope())
         {
-            var iMuseumDbContext = scope.ServiceProvider.GetRequiredService<DbContext>();
+            var iMuseumDbContext = scope.ServiceProvider.GetRequiredService<IMuseumContext>();
             var old = await iMuseumDbContext.Set<Role>().FirstOrDefaultAsync(old => item.Id == old.Id);
             //Check if actually exists an item with that id
             if (old == null)
