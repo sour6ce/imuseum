aa70a6a118aae78008be103c165fd8feb09d4993
deborahfam <deborahfamadas@gmail.com>
2022-06-26 19:54:57 -0400

    commit Adding/IdentityAuth/UnWorking

---
+++ IMuseum.Business/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using IMuseum.Auth.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityAuth.Controllers;
+
+public class HomeController : BaseController
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
