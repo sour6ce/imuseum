86fc5c5733b17dd4968760f596d59274dcd9742d
Sour_ce <gabrielhdezrodriguez@gmail.com>
2022-06-28 03:33:49 -0400

    feat(simple-names): Added mapping functions for museums and rooms

--- IMuseum.Business.Dtos/ConvertionService.cs
+++ IMuseum.Business.Dtos/ConvertionService.cs
@@ -3,6 +3,8 @@ using IMuseum.Business.Dtos.Artworks;
 using IMuseum.Persistence.Models;
 using IMuseum.Persistence.Repositories.Paintings;
 using IMuseum.Persistence.Repositories.Sculptures;
+using IMuseum.Persistence.Repositories.Museums;
+using IMuseum.Persistence.Repositories.Rooms;
 using IMuseum.Business.Dtos.Restorations;
 using IMuseum.Business.Dtos.LoanApplications;
 
@@ -13,11 +15,22 @@ public class ConvertionService : IConvertionService
     private readonly IArtworksRepository artRepository;
     private readonly ISculpturesRepository sculpturesRepository;
     private readonly IPaintingsRepository paintsRepository;
-    public ConvertionService(IArtworksRepository artworks, ISculpturesRepository sculptures, IPaintingsRepository paints)
+    private readonly IRoomsRepository rooms;
+    private readonly IMuseumsRepository museums;
+
+    public ConvertionService(
+        IArtworksRepository artworks,
+        ISculpturesRepository sculptures,
+        IPaintingsRepository paints,
+        IRoomsRepository rooms,
+        IMuseumsRepository museums
+        )
     {
         this.artRepository = artworks;
         this.sculpturesRepository = sculptures;
         this.paintsRepository = paints;
+        this.rooms = rooms;
+        this.museums = museums;
     }
 
     public async Task<ArtworkGeneralDto> ArtworkAsDto(Artwork art)
@@ -182,5 +195,29 @@ public class ConvertionService : IConvertionService
             ArtworkId = loanApp.ArtworkId,
             MuseumId = loanApp.MuseumId
         };
+    }
+
+    public int? MuseumToId(string name)
+    {
+        var r = museums.ExecuteOnDb(x => x.Where(x => x.Name == name).FirstOrDefault());
+        return r?.Id;
+    }
+
+    public string? MuseumFromId(int id)
+    {
+        var r = museums.GetObjectAsync(id).Result;
+        return r?.Name;
+    }
+
+    public int? RoomToId(string name)
+    {
+        var r = rooms.ExecuteOnDb(x => x.Where(x => x.Name == name).FirstOrDefault());
+        return r?.Id;
+    }
+
+    public string? RoomFromId(int id)
+    {
+        var r = rooms.GetObjectAsync(id).Result;
+        return r?.Name;
     }
 }
